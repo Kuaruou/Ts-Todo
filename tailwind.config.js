d2e65548a5145bc66f25d4537056f0495186dd5e
kurokaro <linfanhsin@gmail.com>
2021-10-28 22:39:31 +0800

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayerByDefault: true
+  },
+  purge: ['./public/**/*.html', './src/**/*.vue'],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
